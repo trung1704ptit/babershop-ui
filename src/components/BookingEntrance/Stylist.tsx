5a48f738bd4d9aea7f45ae7f000a337a7d2890bb
Trung Nguyen <trung1704ptit@gmail.com>
2024-05-13 22:18:43 +0800

    change to photo

--- src/components/BookingEntrance/Stylist.tsx
+++ src/components/BookingEntrance/Stylist.tsx
@@ -167,7 +167,7 @@ const Stylist = (props: IProps) => {
             <div className='flex justify-between border-b py-3' key={item.id}>
               <div className='flex text-center items-center'>
                 <Image
-                  src={item?.square_photo || 'default.png'}
+                  src={item.photo}
                   alt='avatar'
                   width='100'
                   height='100'
