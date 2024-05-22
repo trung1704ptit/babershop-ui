5ce708cb419e3b25ccdc4d1fbde83617aed44e39
Trung Nguyen <trung1704ptit@gmail.com>
2024-05-22 22:03:07 +0800

    fix cors

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ module.exports = withBundleAnalyzer({
     return [
       {
         // matching all API routes
-        source: '/api/:path*',
+        source: 'https://api.roybarbershop.com/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
